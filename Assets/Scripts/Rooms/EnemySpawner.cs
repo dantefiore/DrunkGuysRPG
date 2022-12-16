48495a8e953312cbfe36a8fcb0eac9e38d4d7df4
dantefiore <dantefiore17@gmail.com>
2022-12-16 17:17:16 -0500

    Fixed Enemy Stats Duplication
    Fixed the issue where if multiple enemies had the same SO and one of them was killed, all duplicates were killed.

--- Assets/Scripts/Rooms/EnemySpawner.cs
+++ Assets/Scripts/Rooms/EnemySpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -8,6 +9,7 @@ public class EnemySpawner : MonoBehaviour
 
     // the list of posible enemies in the battle
     [SerializeField] List<CharacterStatus> enemies = new List<CharacterStatus>();
+    List<CharacterStatus> posibleEnemies;
 
     [SerializeField] Party party;   // the enemy party
     [SerializeField] MapSaverSO saver;
@@ -21,6 +23,7 @@ public class EnemySpawner : MonoBehaviour
         // is the player didn't spawn from a battle
         if (!saver.fromBattle)
         {
+            RefillList();
             Invoke("SpawnParty", 0.5f);
         }
     }
@@ -50,14 +53,25 @@ public class EnemySpawner : MonoBehaviour
             // adds random enemies to the list in the status manager
             for (int i = 0; i < rand_party_size; i++)
             {
-                int rng = Random.Range(0, enemies.Count);
+                if(posibleEnemies.Count > 0)
+                {
+                    int rng = Random.Range(0, posibleEnemies.Count);
 
-                enemyStatusManager.enemies.Add(enemies[rng]);
+                    enemyStatusManager.enemies.Add(posibleEnemies[rng]);
+                    posibleEnemies.Remove(posibleEnemies[rng]);
+                }
             }
 
             // adds these enemies to the saver lists
             saver.keys.Add(enemyPrefab);
             saver.values.Add(spawnPoints[rand_spawn].position);
+
+            RefillList();
         }
+    }
+
+    void RefillList()
+    {
+        posibleEnemies = new List<CharacterStatus>(enemies);
     }
 }
